8f6deb97b696bb60fecc22e951026c8eaf603a64
ewanouchka <ewanouchka@gmail.com>
2021-07-23 10:50:42 +0200

    revision header, footer, et responsive

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -13,7 +13,7 @@ const displayOneTeddy = async (teddy) => {
           <div class="teddyOptions">
           <h3>Options :</h3> 
           <div class="teddyColor">Coloris disponibles : <div id="teddyColor"></div></div>
-          <div class="teddyQuantity">Quantité : <select id="teddyQuantity"><option>-</option>
+          <div class="teddyQuantity">Quantité : <select id="teddyQuantity" label="quantité"><option>-</option>
           <option>1</option>
           <option>2</option>
           <option>3</option>
