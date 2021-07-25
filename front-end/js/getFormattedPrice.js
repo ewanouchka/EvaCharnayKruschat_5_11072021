8ad31cc45daaa5e9a1a066e0ce9be86cbc6b6fce
ewanouchka <ewanouchka@gmail.com>
2021-07-25 16:41:50 +0200

    suppression fichier inutile

--- front-end/js/getFormattedPrice.js
+++
@@ -1,8 +0,0 @@
-const getFormattedPrice = (format = "fr-FR") => {
-  const euro = new Intl.NumberFormat(format, {
-    style: "currency",
-    currency: "EUR",
-    minimumFractionDigits: 2,
-  });
-  return euro.format(price / 100);
-};
