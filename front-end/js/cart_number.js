a41879aef8c4f5900aecdb4c7572c31f7fbb3852
ewanouchka <ewanouchka@gmail.com>
2021-07-26 14:21:24 +0200

    export fonction affichage nbe de pdts dans panier

---
+++ front-end/js/cart_number.js
@@ -0,0 +1,45 @@
+// fonction affichage du nombre de produits dans le panier dans la barre de nav
+
+// récupération des articles dans le localStorage
+
+const getArticlesCart = async () => {
+  const itemLocalStorage = JSON.parse(localStorage.getItem("selectedArticles"));
+
+  return itemLocalStorage;
+};
+
+// mise en forme des données de l'api pour l'id concernée
+const formatDataCart = async (itemLocalStorage) => {
+  let results = itemLocalStorage;
+
+  const articles = await Promise.all(
+    results.map((articles) => {
+      const { quantity, ...rest } = articles;
+
+      return {
+        quantity,
+        ...rest,
+      };
+    })
+  );
+
+  return articles;
+};
+
+// appel de la fonction affichage du panier
+(async () => {
+  const articles = await formatDataCart(await getArticlesCart());
+
+  const nombreProduits = () => {
+    // fonction calculant le nombre total de produits dans le panier
+    let tableauNombres = [];
+    tableauNombres = articles.map((article) => `${article.quantity}`); // on récupère les valeurs totales pour chaque ligne d'article
+
+    const sommeProduits = tableauNombres.reduce((a, b) => parseInt(a, 10) + parseInt(b, 10)); // on additionne toutes les valeurs de l'array, ramenées en nombres en base 10
+    return sommeProduits;
+  };
+  nombreProduits();
+
+  const cartLogo = document.querySelector("#cartLogo"); // on sélectionne le logo du panier
+  cartLogo.innerHTML += `<span class="cartNumber">` + nombreProduits() + `</span>`; // on affiche le nombre d'articles dans le panier
+})();
