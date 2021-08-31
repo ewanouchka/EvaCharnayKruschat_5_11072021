3a43bdc9cb7a9dfddee3f61201f71c83e5bb70e8
ewanouchka <ewanouchka@gmail.com>
2021-08-31 16:31:14 +0200

    prettier

--- front-end/js/modules/getCart.js
+++ front-end/js/modules/getCart.js
@@ -5,12 +5,13 @@ export let productsInCart = getStorageItem("products");
 export const getCart = () => {
   if (!productsInCart) {
     return [];
-  } 
+  }
 
-  if (!productsInCart[0]) {return Array.of(productsInCart);}
+  if (!productsInCart[0]) {
+    return Array.of(productsInCart);
+  }
 
   return productsInCart;
-  
 };
 
 export let arrayOfProducts = getCart();
