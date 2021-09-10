f19f4dfa94966ea589881f516ce242dfe6faa8da
ewanouchka <ewanouchka@gmail.com>
2021-09-11 01:23:43 +0200

    rectif gestion des erreurs fetch

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,15 +1,10 @@
 import { getTeddies, formatDataTeddies } from "./modules/getTeddies.js";
 import { displayTotalQuantity } from "./modules/getNumberOfProducts.js";
 import { displayTeddies } from "./modules/htmlContent.js";
-import { createErrorMessage } from "./modules/popup.js";
 
 // création de l'objet teddy à afficher
 
-const { teddy } = await formatDataTeddies(
-  await getTeddies("http://localhost:3000/api/teddies").catch((error) => {
-    createErrorMessage(error);
-  })
-);
+const { teddy } = await formatDataTeddies(await getTeddies("http://localhost:3000/api/teddies"));
 
 // appels des fonctions pour affichage
 
--- front-end/js/modules/getTeddies.js
+++ front-end/js/modules/getTeddies.js
@@ -1,30 +1,45 @@
+import { createErrorMessage } from "./popup.js";
+
 // pour la page d'index : accès à la liste des produits du serveur et formattage de l'objet
 
 export const getTeddies = async (url) => {
-  const teddy = await fetch(url);
+  try {
+    const teddy = await fetch(url);
+    const teddyApiJSON = await teddy.json();
 
-  return teddy.json();
+    if (teddyApiJSON) {
+      return teddyApiJSON;
+    }
+  } catch (error) {
+    createErrorMessage("TypeError: Failed to fetch");
+  }
 };
 
 export const formatDataTeddies = async (teddyApiJSON) => {
-  const teddy = await Promise.all(
-    teddyApiJSON.map((teddy) => {
-      const { _id: id, name, price, imageUrl: img, colors, description } = teddy;
+  try {
+    if (teddyApiJSON) {
+      const teddy = await Promise.all(
+        teddyApiJSON.map((teddy) => {
+          const { _id: id, name, price, imageUrl: img, colors, description } = teddy;
+
+          return {
+            id,
+            name,
+            price,
+            img,
+            colors: colors.join(", "),
+            description,
+          };
+        })
+      );
 
       return {
-        id,
-        name,
-        price,
-        img,
-        colors: colors.join(", "),
-        description,
+        teddy,
       };
-    })
-  );
-
-  return {
-    teddy,
-  };
+    }
+  } catch (error) {
+    createErrorMessage("TypeError: Invalid data format");
+  }
 };
 
 // pour la page produit : accès au détail du produit sélectionné en page d'accueil et formattage de l'objet
@@ -33,9 +48,22 @@ export const getOneTeddy = () =>
   getTeddies("http://localhost:3000/api/teddies/" + new URL(location.href).searchParams.get("id"));
 
 export const formatDataOneTeddy = async (oneTeddyJSON) => {
-  const { _id: id, name, price, imageUrl: img, colors, description } = oneTeddyJSON;
+  try {
+    const { _id: id, name, price, imageUrl: img, colors, description } = oneTeddyJSON;
 
-  const teddy = { id, name, price, img, colors, description };
+    const teddy = { id, name, price, img, colors, description };
 
-  return { teddy };
+    const arrayOfTeddyElements = [];
+
+    for (const element in teddy) {
+      arrayOfTeddyElements.push(`${teddy[element]}`);
+    }
+    if (arrayOfTeddyElements.some((value) => value == "undefined")) {
+      createErrorMessage("TypeError: Invalid data format");
+    }
+
+    return { teddy };
+  } catch (error) {
+    createErrorMessage("TypeError: Failed to fetch");
+  }
 };
