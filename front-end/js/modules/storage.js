b9b07a71f9c8b497b040c77d1b80756de3b8f672
ewanouchka <ewanouchka@gmail.com>
2021-09-11 03:26:20 +0200

    suppr console.log + try/catch utilisé pr exemple

--- front-end/js/modules/storage.js
+++ front-end/js/modules/storage.js
@@ -3,13 +3,7 @@ export const getStorageItem = (name) => {
 };
 
 export const setStorageItem = (name, value) => {
-  try {
-    localStorage.setItem(name, JSON.stringify(value));
-    return true;
-  } catch (e) {
-    console.error(e);
-    return false;
-  }
+  localStorage.setItem(name, JSON.stringify(value));
 };
 
 export const removeStorageItem = (name) => {
